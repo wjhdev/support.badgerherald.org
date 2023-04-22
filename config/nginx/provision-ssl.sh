f782143e6d9f5320ccf279e0041bd34817301649
Will Haynes <will@wjh.dev>
2023-04-22 15:03:42 -0500

    replace self signed cert with certbot

--- config/nginx/provision-ssl.sh
+++ config/nginx/provision-ssl.sh
@@ -3,61 +3,56 @@
 # Shell script for generating self-signed cert for Vagrant
 #
 
-FILE=/etc/ssl/private/localhost.key
 
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-    exit
-fi
 
-cd tmp/
-
-DOMAIN=${DOMAIN}
-
-if [ -z "$DOMAIN" ]; then
-  echo "Usage: $(basename $0) <domain>"
-  exit 11
-fi
-
-fail_if_error() {
-  [ $1 != 0 ] && {
-    unset PASSPHRASE
-    exit 10
-  }
-}
-
-# Generate a passphrase
-export PASSPHRASE=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 128; echo)
-
-# Certificate details; replace items in angle brackets with your own info
-subj="
-C=US
-ST=WI
-O=localhost
-localityName=Madison
-commonName=$DOMAIN
-organizationalUnitName=The Badger Herald Alumni Association
-emailAddress=alumni@badgerherald.com
-"
-
-# Generate the server private key
-openssl genrsa -des3 -out $DOMAIN.key -passout env:PASSPHRASE 2048
-fail_if_error $?
-
-# Generate the CSR
-openssl req \
-    -new \
-    -batch \
-    -subj "$(echo -n "$subj" | tr "\n" "/")" \
-    -key $DOMAIN.key \
-    -out $DOMAIN.csr \
-    -passin env:PASSPHRASE
+# Check if the domain name ends in ".test" or is "localhost" and generate our own self-signed certificate
+if [[ $1 =~ \.test$ ]] || [ $1 == "localhost" ]; then
+    cd /tmp/
+    # Define the SSL certificate and key paths
+    CERT_PATH="/etc/letsencrypt/live/support.badgerherald.org/fullchain.pem"
+    KEY_PATH="/etc/letsencrypt/live/support.badgerherald.org/privkey.pem"
     
-# Strip the password so we don't have to type it every time we restart nginx
-openssl rsa -in $DOMAIN.key -out $DOMAIN.key -passin env:PASSPHRASE
+    DOMAIN=$1
+    if [ -z "$DOMAIN" ]; then
+      echo "Usage: $(basename $0) <domain>"
+      exit 11
+    fi
+    fail_if_error() {
+      [ $1 != 0 ] && {
+        unset PASSPHRASE
+        exit 10
+      }
+    }
+    # Generate a passphrase
+    export PASSPHRASE=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 128; echo)
+    # Certificate details; replace items in angle brackets with your own info
+    subj="/C=US/ST=WI/O=localhost/localityName=Madison/commonName=$DOMAIN/organizationalUnitName=The Badger Herald Alumni Association/emailAddress=alumni@badgerherald.com"
+    # Generate the server private key
+    openssl genrsa -des3 -out $DOMAIN.key -passout env:PASSPHRASE 2048
+    fail_if_error $?
+    # Generate the CSR
+    openssl req \
+        -new \
+        -batch \
+        -subj "$(echo -n "$subj" | tr "\n" "/")" \
+        -key $DOMAIN.key \
+        -out $DOMAIN.csr \
+        -passin env:PASSPHRASE
 
-# Generate the cert (good for 10 years)
-openssl x509 -req -days 3650 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
+    # Strip the password so we don't have to type it every time we restart nginx
+    openssl rsa -in $DOMAIN.key -out $DOMAIN.key -passin env:PASSPHRASE
+    # Generate the cert (good for 10 years)
+    openssl x509 -req -days 3650 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
 
-mv $DOMAIN.key /etc/ssl/private/localhost.key
-mv $DOMAIN.crt /etc/ssl/certs/localhost.crt
+    mkdir -p /etc/letsencrypt/live/support.badgerherald.org/
+    
+    # Move the key and cert to their appropriate locations
+    mv $1.key $KEY_PATH
+    mv $1.crt $CERT_PATH
+    
+else
+    echo "Generating LetsEncrypt certificate for $1"
+    
+    # Generate LetsEncrypt certificate using certbot
+    certbot certonly --webroot --webroot-path=/var/www/html -d $1 --non-interactive --agree-tos --email will@broadsheet.technology
+fi
